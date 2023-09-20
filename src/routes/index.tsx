0f75709dfb98178272f4d44f18a04b49df2630fe
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-09-19 23:10:42 -0400

    added image folder to public and fix all links to img dir

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Button } from "~/components/Button/button";
+import { Card } from "~/components/Card/card";
 import { SimpleCard } from "~/components/SimpleCard/simpleCard";
 export default component$(() => {
   return (
     <>
       <section>
         <img
-          src="./image-mockups.png "
+          src="./images/image-mockups.png "
           alt="multiple phones logged into easybank"
         />
         <h2>Next generation digital banking</h2>
@@ -27,14 +28,6 @@ export default component$(() => {
         </p>
 
         <section>
-          <h2>Online Banking</h2>
-          <p>
-            Our modern web and mobile applications allow you to keep track of
-            your finances wherever you are in the world.
-          </p>
-        </section>
-
-        <section>
           <h2>Why choose Easybank?</h2>
           <p>
             We leverage Open Banking to turn your bank account into your
@@ -48,7 +41,7 @@ export default component$(() => {
                 description="
   Our modern web and mobile applications allow you to keep track of your finances 
   wherever you are in the world."
-                image_link="./icon-online.svg"
+                image_link="./images/icon-online.svg"
                 image_alt="digital drawing of a credit card"
               ></SimpleCard>
             </li>
@@ -57,7 +50,7 @@ export default component$(() => {
               <SimpleCard
                 title="Simple Budgeting"
                 description="See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits. "
-                image_link="./icon-budgeting.svg"
+                image_link="./images/icon-budgeting.svg"
                 image_alt="digital drawing of a credit card"
               ></SimpleCard>
             </li>
@@ -65,7 +58,7 @@ export default component$(() => {
               <SimpleCard
                 title="Fast Onboarding"
                 description=" We don’t do branches. Open your account in minutes online and start taking control of your finances right away. "
-                image_link="./icon-onboarding.svg"
+                image_link="./images/icon-onboarding.svg"
                 image_alt="digital drawing of a credit card"
               ></SimpleCard>
             </li>
@@ -74,7 +67,7 @@ export default component$(() => {
               <SimpleCard
                 title="Open API"
                 description=" Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier. "
-                image_link="./icon-api.svg"
+                image_link="./images/icon-api.svg"
                 image_alt="digital drawing of a credit card"
               ></SimpleCard>
             </li>
@@ -84,6 +77,7 @@ export default component$(() => {
 
       <section>
         <h3>Latest Articles</h3>
+        <Card></Card>
       </section>
     </>
   );

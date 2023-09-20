656afd248b47ab60ed80b1b4c64ff353952ef53c
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-09-19 20:27:07 -0400

    added header from FE

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,37 +1,22 @@
-import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
-
 import { component$ } from "@builder.io/qwik";
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
-  const head = useDocumentHead();
-  const loc = useLocation();
-
   return (
     <>
-      <title>{head.title}</title>
-
-      <link rel="canonical" href={loc.url.href} />
+      <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
-      {head.meta.map((m) => (
-        <meta key={m.key} {...m} />
-      ))}
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="./favicon-32x32.png"
+      />
 
-      {head.links.map((l) => (
-        <link key={l.key} {...l} />
-      ))}
-
-      {head.styles.map((s) => (
-        <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
-      ))}
-
-      {head.scripts.map((s) => (
-        <script key={s.key} {...s.props} dangerouslySetInnerHTML={s.script} />
-      ))}
+      <title>Frontend Mentor | Easybank landing page</title>
     </>
   );
 });
